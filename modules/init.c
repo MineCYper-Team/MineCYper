50978ea1c37ad9a9b36d694630376cc2713948b0
Void01702 <93616570+Void01702@users.noreply.github.com>
2022-06-04 10:32:06 +0200

    feat: reveal (with a few bugs)

--- modules/init.c
+++ modules/init.c
@@ -1,9 +1,49 @@
 #include "header.h"
 
+int reveal_bloc(int length, int height, Bloc map[length][height], int abs, int ord){
+    if(map[abs][ord].Revealed){
+      return 0;
+    }
+    else if(map[abs][ord].Value < 0){
+      map[abs][ord].Revealed = 1;
+      map[abs][ord].Value = -2;
+      return -1;
+    }
+    else{
+      map[abs][ord].Value = 0;
+      map[abs][ord].Revealed = 1;
+      if(abs>0 && ord>0 && map[abs-1][ord-1].Value == -1){
+        map[abs][ord].Value++;
+      }
+      if(ord>0 && map[abs][ord-1].Value == -1){
+        map[abs][ord].Value++;
+      }
+      if(abs<length-1 && ord>0 && map[abs+1][ord-1].Value == -1){
+        map[abs][ord].Value++;
+      }
+      if(abs<length-1 && map[abs+1][ord].Value == -1){
+        map[abs][ord].Value++;
+      }
+      if(abs<length-1 && ord<height-1 && map[abs+1][ord+1].Value == -1){
+        map[abs][ord].Value++;
+      }
+      if(ord<height-1 && map[abs][ord+1].Value == -1){
+        map[abs][ord].Value++;
+      }
+      if(abs>0 && ord<height-1 && map[abs-1][ord+1].Value == -1){
+        map[abs][ord].Value++;
+      }
+      if(abs>0 &&  map[abs-1][ord].Value == -1){
+        map[abs][ord].Value++;
+      }
+      return 0;
+    }
+}
+
 void full_reveal_dev(int length, int height, Bloc map[length][height]){
   for(int i=0; i<height; i++){
     for(int j=0; j<length; j++){
-      map[j][i].Revealed = 1;
+      reveal_bloc(length, height, map, j, i);
     }
   }
 }
@@ -46,3 +86,39 @@ int first_turn(int length, int height, Bloc map[length][height], int mines){
   map_print(length, height, map);
   return 0;
 }
+
+
+/*int turn(int length, int height, Bloc map[length][height], int mines, int flags){
+  printf("Flags left: %d\n", flags);
+  map_print(length, height, map);
+  fflush(stdin);
+  int abs;
+	int ord;
+	char txt;
+  char act;
+	int x=0;
+	do{
+		printf("What square do you target?\n");
+		fflush(stdin);
+		do{
+			scanf("%c", &txt);
+		}while(txt<'A' || txt>'A'+length);
+		abs = txt-'A';
+		do{
+			scanf("%d", &ord);
+		}while(ord<1 || ord>height);
+		ord--;
+		if(map[abs][ord].Revealed){
+			printf("This case was already revealed, choose another spot.\n\n");
+		}
+		else{
+			x=1;
+		}
+	}while(x==0);
+  printf("Do you want to reveal (R) or flag (F)?\n");
+  do{
+    scanf("%c", &act);
+  }while(act!='R' && act!='F');
+
+  return 0;
+}*/
